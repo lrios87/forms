a6ef956b656069390a8b36480b17ad3f3230944f
LaTanya Donaldson and louis rios <louis@>
2017-05-31 20:25:31 -0700

    fixed issues works now

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    event.preventDefault();
     var food1 = $("#food").val();
     var music2 = $("#music").val();
     var color3 = $("#color").val();
 
-  $(".foodOutput").text(food1);
-  $(".musicOutput").text(music2);
-  $(".colorOutput").text(color3);
+  $(".foodOutput").append(food1);
+  $(".musicOutput").append(music2);
+  $(".colorOutput").append(color3);
 
+$(".survey").show();
+event.preventDefault();
   });
 });
